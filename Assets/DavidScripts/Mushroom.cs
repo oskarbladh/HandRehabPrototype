0da9e469c48abc998c215415338905043d3cf777
RustyOn <david.petersen98@gmail.com>
2022-10-12 11:07:59 +0200

    ExaminationCanvas(12/10)
    Created a simple UI for the examination. Added two scripts with some basic info, no coding just variables.

---
+++ Assets/DavidScripts/Mushroom.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mushroom : MonoBehaviour
+{
+
+    //Mname = MushroomName
+
+    public string Mname;  //This should be SetName'd for each mushroom type and made into a prefab
+
+    public int Msize; //This should be SetSize'd for each mushroom type and made into a prefab, or randomised if we have time
+
+    public bool isEaten, isInfested, isMiscolored, isMoldy; // Hardcode these into each prefab, I can do it once we have our models - David || or randomised if we have time
+
+
+}
